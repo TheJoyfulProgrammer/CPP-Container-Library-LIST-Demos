7134529e9a37f62a81416be79e1474affba11fe1
Walter Whitman <GitHub@TheJoyfulProgrammer.com>
2020-12-25 02:22:50 -0500

    Update CPP Library Container LIST Demo - v1.cpp

--- CPP Library Container LIST Demo - v1.cpp
+++ CPP Library Container LIST Demo - v1.cpp
@@ -3,7 +3,7 @@
 #include <string>
 #include <algorithm>
 
-namespace USA_PresidentsNS
+namespace USA
 {
     struct President
     {
@@ -17,26 +17,26 @@ namespace USA_PresidentsNS
 int main()
 {
     // Create a list container (a double (bi-directional) linked list) for the US Presidents
-    std::list<USA_PresidentsNS::President> Presidents;
+    std::list<USA::President> Presidents;
 
     // Create an iterator which we can use to find a President in the list
-    std::list<USA_PresidentsNS::President>::iterator PresidentIterator;
+    std::list<USA::President>::iterator PresidentIterator;
     
     // Add some US Presidents to the list
-    Presidents.push_back(USA_PresidentsNS::President{"George", "", "Washington", 1});
-    Presidents.push_back(USA_PresidentsNS::President{"Abraham", "", "Lincoln", 16});
-    Presidents.push_back(USA_PresidentsNS::President{"Ulysses", "S.", "Grant", 18});
-    Presidents.push_back(USA_PresidentsNS::President{"Theodore", "", "Roosevelt", 26});
-    Presidents.push_back(USA_PresidentsNS::President{"Franklin", "D.", "Roosevelt", 32});
+    Presidents.push_back(USA::President{"George", "", "Washington", 1});
+    Presidents.push_back(USA::President{"Abraham", "", "Lincoln", 16});
+    Presidents.push_back(USA::President{"Ulysses", "S.", "Grant", 18});
+    Presidents.push_back(USA::President{"Theodore", "", "Roosevelt", 26});
+    Presidents.push_back(USA::President{"Franklin", "D.", "Roosevelt", 32});
     
     // Find a President in the list and use a Lamda.
     PresidentIterator = std::find_if(Presidents.begin(), Presidents.end(), 
-                        [](const USA_PresidentsNS::President& ListItem) {
+                        [](const USA::President& ListItem) {
                         return (ListItem.FirstName == "Franklin") & (ListItem.LastName == "Roosevelt"); });
     
     // Insert a President in the list
     if (PresidentIterator != Presidents.end()) {
-        Presidents.insert(PresidentIterator, USA_PresidentsNS::President{"Woodrow", "", "Wilson", 28});
+        Presidents.insert(PresidentIterator, USA::President{"Woodrow", "", "Wilson", 28});
     }
     
     Presidents.erase(PresidentIterator);
@@ -55,5 +55,3 @@ int main()
                   << "\n";
     }
 }
-
-
